2247c7687b6d1030a14d9a5aad8094d1d59caf4d
AndyWilliams1408 <134110811+AndyWilliams1408@users.noreply.github.com>
2023-08-25 15:54:02 -0500

    a

---
+++ Proyecto1/script.js
@@ -0,0 +1,106 @@
+let totalValue = 0;
+
+function registrarCupo() {
+    const fecha = document.getElementById("fecha").value;
+    const hora = document.getElementById("hora").value;
+    const valor = parseFloat(document.getElementById("valor").value);
+    const recibe = document.getElementById("recibe").value;
+    const pagoPor = document.getElementById("pagoPor").value;
+    const pagoD = document.getElementById("pagoPor");
+
+    // registro de cupo
+
+    totalValue += valor;
+    document.getElementById("totalValue").textContent = totalValue;
+    const newRow = transaccionesTableBody.insertRow();
+    newRow.insertCell().textContent = fecha;
+    newRow.insertCell().textContent = hora;
+    newRow.insertCell().textContent = valor;
+    newRow.insertCell().textContent = recibe;
+    newRow.insertCell().textContent = pagoPor;
+
+    var newClient = pagoD.value;
+            if (newClient !== '') {
+                var option = document.createElement('option');
+                option.value = newClient;
+                option.textContent = newClient;
+                clientSelect.appendChild(option);
+                pagoD.value = '';
+            }
+}
+
+const transaccionesTableBody = document.getElementById("transaccionesTableBody");
+const emprendimientosList = document.getElementById("emprendimientosList");
+const fundacionList = document.getElementById("fundacionList");
+
+clientSelect.addEventListener('change', function() {
+        var selectedClient = clientSelect.value;
+    
+    if (selectedClient === 'op1') {
+        console.log("Hola")
+        alert('Seleccione un cliente antes de agregarlo a la tabla.');
+        return; // Detener la ejecución si no se ha seleccionado un cliente válido
+    }
+ })
+
+    function actualizarCupo() { 
+   
+    
+    const fecha = document.getElementById("fechaActualizacion").value;
+    const hora = document.getElementById("horaActualizacion").value;
+    const valorAnterior = parseFloat(document.getElementById("valorAnterior").value);
+    const valorActual = parseFloat(document.getElementById("valorActual").value);
+    const empleado = document.getElementById("empleado").value;
+    const cliente = document.getElementById("clientSelect").value;
+    
+    // actualización de cupo 
+    totalValue -= valorAnterior; // Resta el valor anterior
+    totalValue += valorActual;    // Suma el valor actual
+    document.getElementById("totalValue").textContent = totalValue;
+
+    // tabla de versionamiento
+    const newRow = transaccionesTableBody.insertRow();
+    newRow.insertCell().textContent = fecha;
+    newRow.insertCell().textContent = hora;
+    newRow.insertCell().textContent = valorAnterior + " --> " + valorActual;
+    newRow.insertCell().textContent = empleado;
+    newRow.insertCell().textContent = cliente + "(Actualizado)";
+    
+    }
+    
+    
+
+// 
+
+function listarValoresEmprendimientos() {
+    const emprendimientos = [
+        { nombre: "Emprendimiento 1", valor: 500 },
+        { nombre: "Emprendimiento 2", valor: 800 },
+    ];
+
+    emprendimientosList.innerHTML = ""; // Limpia la lista antes de actualizar
+
+    emprendimientos.forEach(emprendimiento => {
+        const li = document.createElement("li");
+        li.textContent = `${emprendimiento.nombre}: $${emprendimiento.valor}`;
+        emprendimientosList.appendChild(li);
+    });
+}
+
+function listarValoresFundacion() {
+    const donacionesFundacion = [
+        { motivo: "Donación 1", valor: 200 },
+        { motivo: "Donación 2", valor: 300 },
+    ];
+
+    fundacionList.innerHTML = ""; // Limpia la lista antes de actualizar
+
+    donacionesFundacion.forEach(donacion => {
+        const li = document.createElement("li");
+        li.textContent = `${donacion.motivo}: $${donacion.valor}`;
+        fundacionList.appendChild(li);
+    });
+}
+
+
+
